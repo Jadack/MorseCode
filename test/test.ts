7d52909588c98fac144a16208adde5d4a7793bc1
Jadack <jackdamiolixp@gmail.com>
2021-10-12 18:47:15 -0400

    Morse Decode Unit Test

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,8 @@
 import { MorseCode } from '../src/app';
 import { expect } from 'chai';
 
-describe('MorseCode', () => {
-
-  // it('Should return an invalid message with invalid input', () => {
-  //   let morseCode = new MorseCode();
-  //   expect(morseCode.morseEncode(123535)).to.equal('Invalid Input');
-  // });
-
+describe('MorseCode Encode', () => {
+  
   it('Should return an invalid message with invalid separator (°)', () => {
     let morseCode = new MorseCode();
     expect(morseCode.morseEncode('Hello', '°')).to.equal('Invalid Separator');
@@ -127,5 +122,130 @@ describe('MorseCode', () => {
   it('Should return valid value with integrated separator (|....| |.| |.-..| |.-..| |---|  |.--| |---| |.-.| |.-..| |-..|) with Hello World word', () => {
     let morseCode = new MorseCode();
     expect(morseCode.morseEncode('Hello World', '--pipe')).to.equal('|....| |.| |.-..| |.-..| |---|  |.--| |---| |.-.| |.-..| |-..|');
+  });
+});
+
+describe('MorseCode Decode', () => {
+
+  it('Should return an empty string with empty input', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('')).to.equal('');
+  });
+
+  it('Should return an empty string with invalid characters', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('# $ % ! + = ? < > ~ | { } [ ] * ^')).to.equal('');
+  });
+
+  it('Should return only valid values despite the string has invalid characters', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('# $ % ! + = ? < > ~ | { } [ ] * ^ .... . .-.. .-.. ---')).to.equal('hello');
+  });
+
+  // Space is the default separator
+  it('Should return Hello word with default separator (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---')).to.equal('hello');
+  });
+
+  it('Should return Hello World with default separator (.... . .-.. .-.. ---  .-- --- .-. .-.. -..)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..')).to.equal('hello world');
+  });
+
+  it('Should return 22 10 with default separator (..--- ..---  .---- -----)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('..--- ..---  .---- -----')).to.equal('22 10');
+  });
+
+  // Custom separator is the user indication
+  it('Should return Hello with custom separator (...././.-../.-../---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('...././.-../.-../---')).to.equal('hello');
+  });
+
+  it('Should return Hello World with custom separator (...././.-../.-../--- .--/---/.-./.-../-..)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('...././.-../.-../---/ .--/---/.-./.-../-..')).to.equal('hello world');
+  });
+
+  // Integrated separator Decoder
+
+  // Parenthesis
+  it('Should return hello with integrated separator ((....) (.) (.-..) (.-..) (---))', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('(....) (.) (.-..) (.-..) (---)')).to.equal('hello');
+  });
+
+  it('Should return hello world with integrated separator ((....) (.) (.-..) (.-..) (---)  (.--) (---) (.-.) (.-..) (-..))', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('(....) (.) (.-..) (.-..) (---)  (.--) (---) (.-.) (.-..) (-..)')).to.equal('hello world');
+  });
+
+  // Sharp
+  it('Should retur hello with integrated separator (#....# #.# #.-..# #.-..# #---#)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('#....# #.# #.-..# #.-..# #---#')).to.equal('hello');
+  });
+
+  it('Should return hello world with integrated separator (#....# #.# #.-..# #.-..# #---#  #.--# #---# #.-.# #.-..# #-..#)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('#....# #.# #.-..# #.-..# #---#  #.--# #---# #.-.# #.-..# #-..#')).to.equal('hello world');
+  });
+
+  // Asterisk
+  it('Should return hello with integrated separator (*....* *.* *.-..* *.-..* *---*)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('*....* *.* *.-..* *.-..* *---*')).to.equal('hello');
+  });
+
+  it('Should return hello world with integrated separator (*....* *.* *.-..* *.-..* *---*  *.--* *---* *.-.* *.-..* *-..*)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('*....* *.* *.-..* *.-..* *---*  *.--* *---* *.-.* *.-..* *-..*')).to.equal('hello world');
+  });
+
+  // Pipe
+  it('Should return hello with integrated separator (|....| |.| |.-..| |.-..| |---|)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('|....| |.| |.-..| |.-..| |---|')).to.equal('hello');
+  });
+
+  it('Should return hello world with integrated separator (|....| |.| |.-..| |.-..| |---|  |.--| |---| |.-.| |.-..| |-..|)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('|....| |.| |.-..| |.-..| |---|  |.--| |---| |.-.| |.-..| |-..|')).to.equal('hello world');
+  });
+
+  // Text conventions - LowerCase as Default
+
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '-lc')).to.equal('hello world');
+  });
+
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '--lowerCase')).to.equal('hello world');
+  });
+
+  // UpperCase
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '-uc')).to.equal('HELLO WORLD');
+  });
+
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '--upperCase')).to.equal('HELLO WORLD');
+  });
+
+  // PascalCase
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '-pc')).to.equal('Hello World');
+  });
+
+  it('Should return Hello World with default text convention (.... . .-.. .-.. ---)', () => {
+    let morseCode = new MorseCode();
+    expect(morseCode.morseDecode('.... . .-.. .-.. ---  .-- --- .-. .-.. -..', '--pascalCase')).to.equal('Hello World');
   });
 });
